f66d1ccf58e74c95f42f660116f3e9ea2dce712b
Mohaned Giess <mohaneduestc@gmail.com>
2020-04-07 10:46:09 +0200

    The project

---
+++ gatsby-personal-site-2019-starter/src/components/Courses/Courses.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Course from "./Course"
+import {graphql, useStaticQuery} from "gatsby"
+
+
+const Courses = () => {
+    return (
+        <div>
+            Hello From Courses
+        </div>
+    )
+}
+
+export default Courses
